08aab1fb3645fb9cb577e30dbec951abf2ec1617
Jordi Mas <jmas@softcatala.org>
2020-02-10 21:17:27 +0100

    Install

---
+++ install.sh
@@ -0,0 +1,14 @@
+sudo apt-get update -y
+pip install tensorflow-addons
+pip install tensorflow-gpu --ignore-installed wrapt
+pip install OpenNMT-tf --ignore-installed PyYAML
+
+
+git clone https://github.com/OpenNMT/OpenNMT-py.git
+cd OpenNMT-py
+git checkout 0.9.0
+
+cd ..
+mkdir model1
+cd model1
+
