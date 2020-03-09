41dc28ee593a56884ef3712f42f2a2e1102c71ab
Jordi Mas <jmas@softcatala.org>
2020-03-09 21:03:44 +0100

    More

--- eng-cat/voc.sh
+++ eng-cat/voc.sh
@@ -1,2 +1,2 @@
-onmt-build-vocab --size 50000 --save_vocab src-vocab.txt src-train.txt
-onmt-build-vocab --size 50000 --save_vocab tgt-vocab.txt tgt-train.txt
+onmt-build-vocab --size 100000 --save_vocab src-vocab.txt src-train.txt
+onmt-build-vocab --size 100000 --save_vocab tgt-vocab.txt tgt-train.txt
