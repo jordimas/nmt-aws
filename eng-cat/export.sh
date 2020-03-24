790b5443d772a7ff2d3bc66a939cc9544e820e7a
Jordi Mas <jmas@softcatala.org>
2020-03-24 17:39:53 +0000

    Export with more data

--- eng-cat/export.sh
+++ eng-cat/export.sh
@@ -1,3 +1,9 @@
+echo   rm -f exported.zip
+currentDate=`date +"%Y-%m-%d-%s"`
+read -p 'Describe model: ' uservar
 onmt-main --config data.yml --auto_config export --export_dir exported/
-cp *vocab* exported/
-zip -r exported.zip exported/
+echo "Model description: $uservar" >  exported/assets/model_description.txt
+ls *.txt -l > exported/assets/inputs_used.txt
+cp *.model exported/assets/
+cp data.yml exported/assets/
+zip -r exported-$currentDate.zip exported/
