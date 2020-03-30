0adc590d0b444dc6b70b0ce2fae377b677b0bb45
Jordi Mas <jmas@softcatala.org>
2020-03-30 10:48:00 +0200

    Bleu calculation

--- eng-cat/bleu.sh
+++ eng-cat/bleu.sh
@@ -1,3 +1,4 @@
-onmt-main --config data.yml --auto_config infer --features_file src-test.txt.token > predictions.txt
-perl ../OpenNMT-py/tools/multi-bleu.perl tgt-test.txt.token < predictions.txt 
-
+onmt-main --config data.yml --auto_config infer --features_file src-test.txt.token > predictions.txt.token
+perl ../OpenNMT-py/tools/multi-bleu.perl tgt-test.txt.token < predictions.txt.token 
+python3 ../sentencepiece-bleu.py
+perl ../OpenNMT-py/tools/multi-bleu.perl tgt-test.txt < predictions.txt
--- sc/bleu.sh
+++ sc/bleu.sh
@@ -1,4 +1,4 @@
 onmt-main --config data.yml --auto_config infer --features_file src-test.txt.token > predictions.txt.token
 perl ../OpenNMT-py/tools/multi-bleu.perl tgt-test.txt.token < predictions.txt.token 
-python3 sentencepiece-bleu.py
+python3 ../sentencepiece-bleu.py
 perl ../OpenNMT-py/tools/multi-bleu.perl tgt-test.txt < predictions.txt
