d65789ed141f1b988cba7543426358c70a59e68f
Jordi Mas <jmas@softcatala.org>
2020-04-04 07:33:39 +0000

    Add bleu to model desc

--- eng-cat/bleu.sh
+++ eng-cat/bleu.sh
@@ -1,4 +1,5 @@
 onmt-main --config data.yml --auto_config infer --features_file src-test.txt.token > predictions.txt.token
 perl ../OpenNMT-py/tools/multi-bleu.perl tgt-test.txt.token < predictions.txt.token 
 python3 ../sentencepiece-bleu.py
-perl ../OpenNMT-py/tools/multi-bleu.perl tgt-test.txt < predictions.txt
+perl ../OpenNMT-py/tools/multi-bleu.perl tgt-test.txt < predictions.txt > bleu.txt
+cat bleu.txt
