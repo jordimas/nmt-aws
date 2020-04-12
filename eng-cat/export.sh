00e09bd0738e5eeb1a56c7d841670c1c70875466
Jordi Mas <jmas@softcatala.org>
2020-04-12 10:34:10 +0000

    Date in model desc

--- eng-cat/export.sh
+++ eng-cat/export.sh
@@ -1,9 +1,11 @@
 echo   rm -f exported.zip
+modelDescription="exported/assets/model_description.txt"
 currentDate=`date +"%Y-%m-%d-%s"`
 read -p 'Describe model: ' uservar
 onmt-main --config data.yml --auto_config export --export_dir exported/
-echo "Model description: $uservar" >  exported/assets/model_description.txt
-cat bleu.txt >>  exported/assets/model_description.txt
+echo "Model description: $uservar" >  $modelDescription
+cat bleu.txt >>  $modelDescription
+echo "Date: $currentDate" >> $modelDescription
 ls *.txt -l > exported/assets/inputs_used.txt
 cp *.model exported/assets/
 cp data.yml exported/assets/
