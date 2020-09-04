eb2887770c2bef19493883e909032f4a14572020
Jordi Mas <jmas@softcatala.org>
2020-09-04 13:10:21 +0200

    src

--- install-cuda.sh
+++ install-cuda.sh
@@ -1,3 +1,5 @@
+# Taken from https://www.tensorflow.org/install/gpu
+
 #Add NVIDIA package repositories
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
