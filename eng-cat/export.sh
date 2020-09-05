6a4fe34eedda20764b7c8e91f2b19c87b166bed2
Jordi Mas <jmas@softcatala.org>
2020-09-05 19:27:05 +0200

    dir struct

--- eng-cat/export.sh
+++ eng-cat/export.sh
@@ -1,15 +1,19 @@
-echo   rm -f exported.zip
-modelDescription="exported/assets/model_description.txt"
+rm -f exported.zip
+rm exported/ -r -f
+mkdir -p exported/metadata
+mkdir -p exported/tokenizer
+
+modelDescription="exported/metadata/model_description.txt"
 currentDate=`date +"%Y-%m-%d-%s"`
 read -p 'Describe model: ' uservar
-onmt-main --config data.yml --auto_config export --export_dir exported/
+onmt-main --config data.yml --auto_config export --export_dir exported/tensorflow/
 echo "Model description: $uservar" >  $modelDescription
 cat bleu.txt >>  $modelDescription
 echo "Date: $currentDate" >> $modelDescription
-ls *.txt -l > exported/assets/inputs_used.txt
-cp *.model exported/assets/
-cp data.yml exported/assets/
-cp src-vocab.txt.token exported/assets/
-cp tgt-vocab.txt.token exported/assets/
+ls *.txt -l > exported/metadata/inputs_used.txt
+cp *.model exported/tokenizer/
+cp data.yml exported/metadata/
+cp src-vocab.txt.token exported/tensorflow/assets/
+cp tgt-vocab.txt.token exported/tensorflow/assets/
 onmt-main --config data.yml --auto_config export --export_dir exported/ctranslate2 --export_format ctranslate2
-zip -r exported-$currentDate.zip exported/
+cd exported && zip -r exported-$currentDate.zip *
