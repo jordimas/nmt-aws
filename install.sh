9c600f9f60fd53a474ac97553cb3614ecfe4ff41
Jordi Mas <jmas@softcatala.org>
2020-10-21 22:47:31 +0200

    Remove model1

--- install.sh
+++ install.sh
@@ -10,9 +10,6 @@ git clone https://github.com/OpenNMT/OpenNMT-py.git
 cd OpenNMT-py
 git checkout -b 0.9.0
 
-cd ..
-mkdir model1
-cd model1
 git config --global user.name "Jordi Mas"
 git config --global user.email jmas@softcatala.org
 
