42e18c4b7197b34f0249ba20103544d30e361eb0
Jordi Mas <jmas@softcatala.org>
2020-11-15 19:22:03 +0100

    Fixes

--- eng-cat/voc.sh
+++ eng-cat/voc.sh
@@ -1,4 +1,3 @@
 VOCSIZE=50000
-python3 ../sentencepiece-tokenizer.py -v $VOCSIZE
-onmt-build-vocab --size $VOCSIZE --save_vocab src-vocab.txt.token src-train.txt.token
-onmt-build-vocab --size $VOCSIZE --save_vocab tgt-vocab.txt.token tgt-train.txt.token
+onmt-build-vocab --sentencepiece --size $VOCSIZE --save_vocab src-vocab.txt src-train.txt
+onmt-build-vocab --sentencepiece --size $VOCSIZE --save_vocab tgt-vocab.txt tgt-train.txt
--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #sudo apt-get update -y
 #sudo apt-get upgrade -y
-pip install tensorflow-gpu --ignore-installed wrapt
-pip install tensorflow-addons
-pip install OpenNMT-tf==2.7.0 --ignore-installed PyYAML
+pip install tensorflow==2.3.1
+#pip install tensorflow-addons
+pip install OpenNMT-tf==2.13.0
 sudo apt-get install zip -y
 
 
@@ -13,3 +13,4 @@ git checkout -b 0.9.0
 git config --global user.name "Jordi Mas"
 git config --global user.email jmas@softcatala.org
 
+bash versions.sh
