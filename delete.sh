fff41e62e895ee57fef98427da4e966407afe174
Jordi Mas <jmas@softcatala.org>
2020-11-19 11:29:46 +0000

    Delete

--- delete.sh
+++ delete.sh
@@ -2,3 +2,5 @@
 echo Deletes unnecessary files before training
 sudo rm -r -f /var/cache/apt/archives/
 sudo rm -r -f /usr/local/cuda-11.0
+sudo rm -r -f /home/ubuntu/anaconda3/envs/aws_neuron* 
+sudo rm -r -f /home/ubuntu/anaconda3/envs/pytor* 
